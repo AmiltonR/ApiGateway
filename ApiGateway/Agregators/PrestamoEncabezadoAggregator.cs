2580184c4a04ca2451b7cace4da95b6a99d288cb
AmiltonR <32588294+AmiltonR@users.noreply.github.com>
2022-12-01 23:55:49 -0600

    delete method detalle prestamo

--- ApiGateway/Agregators/PrestamoEncabezadoAggregator.cs
+++ ApiGateway/Agregators/PrestamoEncabezadoAggregator.cs
@@ -5,6 +5,8 @@ using Ocelot.Middleware;
 using Ocelot.Multiplexer;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http;
+using Ocelot.Responses;
 
 namespace ApiGateway.Agregators
 {
@@ -18,29 +20,38 @@ namespace ApiGateway.Agregators
                 var prestamo = await responses[0].Items.DownstreamResponse().Content.ReadFromJsonAsync<List<EncabezadoPrestamoDTO>>();
                 var usuarios = await responses[1].Items.DownstreamResponse().Content.ReadFromJsonAsync<List<UsuariosDTO>>();
 
-                EncabezadoPrestamoRespuestaDTO response = new EncabezadoPrestamoRespuestaDTO();
-
-                prestamo.ForEach(p =>
+                if (prestamo.Count != 0)
                 {
-                    response.Estudiante.AddRange(usuarios.Where(a => a.Id == p.IdUsuario));
-                    response.Bibliotecario.AddRange(usuarios.Where(a => a.Id == p.IdUsuarioBibliotecario));
-                    //break cuando se encuentren
-                });
+                    EncabezadoPrestamoRespuestaDTO response = new EncabezadoPrestamoRespuestaDTO();
 
-                response.Id = prestamo[0].Id;
-                response.IdUsuario = prestamo[0].IdUsuario;
-                response.IdUsuarioBibliotecario = prestamo[0].IdUsuarioBibliotecario;
-                response.FechaHora = prestamo[0].FechaHora;
+                    prestamo.ForEach(p =>
+                    {
+                        response.Estudiante.AddRange(usuarios.Where(a => a.Id == p.IdUsuario));
+                        response.Bibliotecario.AddRange(usuarios.Where(a => a.Id == p.IdUsuarioBibliotecario));
+                        //break cuando se encuentren
+                    });
+
+                    response.Id = prestamo[0].Id;
+                    response.IdUsuario = prestamo[0].IdUsuario;
+                    response.IdUsuarioBibliotecario = prestamo[0].IdUsuarioBibliotecario;
+                    response.FechaHora = prestamo[0].FechaHora;
 
 
-                var jsonString = JsonConvert.SerializeObject(response, Formatting.Indented, new JsonConverter[] { new StringEnumConverter() });
+                    var jsonString = JsonConvert.SerializeObject(response, Formatting.Indented, new JsonConverter[] { new StringEnumConverter() });
 
-                var stringContent = new StringContent(jsonString)
-                {
-                    Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
-                };
+                    var stringContent = new StringContent(jsonString)
+                    {
+                        Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+                    };
+                    return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
+                }
 
-                return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
+                string r = "No hay Datos";
+                var jsonString1 = JsonConvert.SerializeObject(r, Formatting.Indented, new JsonConverter[] { new StringEnumConverter() });
+
+                var stringContent1 = new StringContent(jsonString1);
+
+                return new DownstreamResponse(stringContent1, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
             }
             catch (Exception)
             {
