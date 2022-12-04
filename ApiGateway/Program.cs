7e579d2c468ac70f8f52468a44b69e465c8c23e4
AmiltonR <32588294+AmiltonR@users.noreply.github.com>
2022-12-04 00:40:50 -0600

    ocelot.json

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -109,7 +109,7 @@ builder.Services.AddCustomJwtAuthentication();
 var app = builder.Build();
 
 //Set environment 
-app.Environment.EnvironmentName = "Development";
+app.Environment.EnvironmentName = "Production";
 
 // Configure the HTTP request pipeline.
 
